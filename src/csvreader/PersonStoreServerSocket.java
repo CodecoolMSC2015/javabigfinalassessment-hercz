f70cc4b9de2d996d7dbadb9a7484be1e7aebf195
Herczku Misi <kmisku@gmail.com>
2016-03-16 15:21:38 +0100

    cocket-client connection added

--- src/csvreader/PersonStoreServerSocket.java
+++ src/csvreader/PersonStoreServerSocket.java
@@ -1,10 +1,44 @@
 package csvreader;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class PersonStoreServerSocket {
 
 	public void start() {
+
+		try {
+
+			System.out.println("---SERVER---");
+			ServerSocket serverSocket = new ServerSocket(1555);
+			System.out.println("\nserver is waiting...");
+
+			Socket pipe = serverSocket.accept();
+
+			System.out.println("Client Connected!");
+
+			ObjectInputStream serverInputStream = new ObjectInputStream(pipe.getInputStream());
+			ObjectOutputStream serverOutputStream = new ObjectOutputStream(pipe.getOutputStream());
+
+			String searchCriteria = (String) serverInputStream.readObject();
+			String searchType = (String) serverInputStream.readObject();
+			System.out.println(searchCriteria + searchType);
+
+			serverInputStream.close();
+			serverOutputStream.close();
+			serverSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	DataReader dataReader = new DataReader() {
 	};
+
+	public static void main(String[] args) {
+		PersonStoreServerSocket p = new PersonStoreServerSocket();
+		p.start();
+	}
 }
--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -1,33 +1,51 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SearchServlet
- */
-@WebServlet("/SearchServlet")
+//@WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public void send(String text) {
+		try {
+			Socket clientSocket = new Socket("localhost", 1555);
+
+			ObjectOutputStream clientOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+
+			clientOutputStream.writeObject(text);
+			clientSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String searchType, skills;
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		skills = (String) request.getParameter("skills");
+		searchType = (String) request.getParameter("searchType");
+		send(skills);
+		send(searchType);
+
+		out.println(skills + searchType);
+		request.getRequestDispatcher("index.html").include(request, response);
+		send(searchType);
+
+		out.close();
 	}
 
 }
