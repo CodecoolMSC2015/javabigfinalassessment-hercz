124b083a4198f0a52840e6dc5ce7bd29d3097b54
Herczku Misi <kmisku@gmail.com>
2016-03-16 11:35:24 +0100

    persondetails getters, setters, other basic things added

--- src/persondetails/Employee.java
+++ src/persondetails/Employee.java
@@ -1,5 +1,28 @@
 package persondetails;
 
-public class Employee {
+public class Employee extends Person {
+	public int salary;
+	public String jobTitle;
 
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	@Override
+	public String toString() {
+
+		return super.toString();
+	}
 }
--- src/persondetails/Skill.java
+++ src/persondetails/Skill.java
@@ -1,5 +1,25 @@
 package persondetails;
 
 public class Skill {
+	public String name;
+	public String description;
+	public double rate;
+
+	public Skill(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getRate() {
+		return rate;
+	}
 
 }
