328eefd601db829580b211abdf89412071b8dff2
Herczku Misi <kmisku@gmail.com>
2016-03-16 13:41:11 +0100

    csvreader method added

--- src/csvreader/CSVDataReader.java
+++ src/csvreader/CSVDataReader.java
@@ -1,18 +1,37 @@
 package csvreader;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Set;
 
 import persondetails.Person;
 
 public class CSVDataReader extends DataReader {
-	private String csvFilePath;
-	private List<Person> persons;
+	String csvFilePath;
+	Set<Person> persons;
 
 	public CSVDataReader(String csvFilePath) {
+		BufferedReader br = null;
+		String line = "";
+		String csvSplitBy = ",";
+
+		try {
+			br = new BufferedReader(new FileReader(csvFilePath));
+			while ((line = br.readLine()) != null) {
+				String[] text = line.split(csvSplitBy);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public List<Person> getPersons() {
+	public Set<Person> getPersons() {
 		return persons;
 	}
 
+	// public static void main(String[] args) {
+	// new CSVDataReader(
+	// "C:\\Users\\herczkumihalybalazs\\git\\javabigfinalassessment-hercz\\Documentation\\persons.csv");
+	// }
 }
